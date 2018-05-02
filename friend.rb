f3c80c52530d62210b74513bbb91abe76fe4a568
behrami <behram1993@gmail.com>
2018-05-02 14:46:31 -0400

    another way

--- friend.rb
+++ friend.rb
@@ -4,14 +4,18 @@ class Friend
     @name
   end
 
-  def greet(*args)
+  # def greet(*args)
+  #
+  #   if args.size == 0
+  #     return "Hello !"
+  #   elsif args.size == 1
+  #     return "Hello #{args[0]}!"
+  #   end
+  #
+  # end
 
-    if args.size == 0
-      return "Hello !"
-    elsif args.size == 1
-      return "Hello #{args[0]}!"
-    end
-
+  def greet(name = nil)
+    "Hello #{name}!"
   end
 
 end
